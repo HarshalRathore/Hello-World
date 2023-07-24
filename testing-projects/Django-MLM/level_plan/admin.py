73453d1fe3365ef154de9102e7ca49c225642277
Harshal Rathore <harshalrathore2014@gmail.comm>
2023-07-24 18:14:01 +0530

    update

--- testing-projects/Django-MLM/level_plan/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from level_plan.models import MLMLevelPlan
-from level_plan.models import Plans
-
-# Register your models here.
-admin.site.register(MLMLevelPlan)
-admin.site.register(Plans)
